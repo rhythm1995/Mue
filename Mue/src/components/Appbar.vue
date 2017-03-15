4c1aad6e52da420948888034f3249c27b200f14b
bugzhang <flyhighdream@qq.com>
2017-03-16 06:24:52 +0800

    no message

---
+++ Mue/src/components/Appbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
